ec1b146a2e77df744db3f57261c9911b0c376320
valktaelen <alix.artiges@hotmail.fr>
2022-10-07 00:02:10 +0200

    IN PROGRESS select

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -33,7 +33,6 @@ Server::~Server() {}
  */
 void	Server::connect() {
 	// Create socket
-
 	_sockServ = socket(AF_INET, SOCK_STREAM, 0);
 	if (_sockServ < 0)
 		throw std::runtime_error("error create socket server");
@@ -44,33 +43,55 @@ void	Server::connect() {
 	_sockAddr.sin_family = AF_INET;
 	_sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	_sockAddr.sin_port = htons(_port);
+
+
 	// Run server
 	if (bind(_sockServ, (struct sockaddr*)&_sockAddr, sizeof(_sockAddr)) == -1
 		|| listen(_sockServ, 100) == -1)
 		throw std::runtime_error(strerror(errno));
 	if (DEBUG)
 		std::cout << "Server open on port " << _port << std::endl;
-		fd_set	read;
-		fd_set	write;
-		FD_ZERO(&read);
-		FD_ZERO(&write);
-		FD_SET(_sockServ, &read);
+
+	struct timeval	tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 1000;
+	int max_fd = _sockServ + 1;
+	fd_set	currentSocket, readySocket;
+	FD_ZERO(&currentSocket);
+	FD_SET(_sockServ, &currentSocket);
 	while (true)
 	{
-		std::cout << (_clients.empty() ? _sockServ : (_clients.end())->first) + 1 << std::endl;
-		int activity = select(, &read, NULL, NULL, NULL);
-		std::cout << activity << std::endl;
-		std::cout << "foo" << std::endl;
-		if (activity < 0)
+		readySocket = currentSocket;
+		if (select(max_fd, &readySocket, NULL, NULL, NULL) < 0)
 			throw std::runtime_error(strerror(errno));
-		int client = accept(_sockServ, NULL, NULL);
-		if (client >= 0) {
-			if (DEBUG)
-				std::cout << "Connection new client" << std::endl;
-			_clients.insert(pair_type(client, client));
-			for (map_iterator it = _clients.begin(); it != _clients.end(); it++)
-				std::cout << it->first << " " << it->second << std::endl;
-			std::cout << std::endl;
+		std::cout << max_fd <<std::endl;
+		for (int i = 0; i < max_fd; i++) {
+			if (FD_ISSET(i, &readySocket)) {
+				if (i == _sockServ) {
+					// new
+					int newSocket = accept(_sockServ, NULL, NULL);
+					if (newSocket >= 0) {
+						if (DEBUG)
+							std::cout << "fd: " << newSocket << " Connection new client" << std::endl;
+					}
+					else
+						throw std::runtime_error(strerror(errno)); // TODO rm
+					if (i >= max_fd)
+						max_fd = i + 1;
+				}
+				else {
+					// TODO taff
+					char buff[5000];
+						recv(i, &buff, sizeof(buff), 0);
+					
+					// send command for client
+				    //send(_server, &buff, sizeof(std::string), 0);
+				        std::cout << "fd: " << i << " \n" << buff << std::endl;
+					FD_CLR(i, &currentSocket);
+				}
+			}else std::cout << "shit\n";
 		}
 	}
 }
+
+
