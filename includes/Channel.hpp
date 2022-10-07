6ee003739102d89d8e9567bcfa9ba578c3da7b2d
Romain Cuminal <rcuminal@z2r4p6.42lyon.fr>
2022-10-08 01:25:02 +0200

    push maj avant merge 2

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -4,7 +4,13 @@
 
 namespace irc { 
 	class Channel {
-
+		private:
+				std::string		_channelName;
+				bool			_private;
+				// Map des utilisateurs
+				// Map des moderateurs
+				// Map des channels
+				
 		public:
 				Channel() {};
 				~Channel() {};
